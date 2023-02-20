ecdf4f2c7727d8b301238094a98aa7ac98a9b8ae
YorkieFl <44997756+YorkieFl@users.noreply.github.com>
2023-02-20 10:03:03 +0000

    Update SimpleVMSize.ps1
    actually makes the tagging for dynamic work by comparing the current size vs the max size

--- SimpleVMSize.ps1
+++ SimpleVMSize.ps1
@@ -3,12 +3,17 @@ Get-VM | ForEach-Object {
   $vm = $_
   Get-VMHardDiskDrive -VMName $vm.Name | ForEach-Object {
     $vhd = Get-VHD $_.Path
+    if ($vhd.FileSize -lt $vhd.Size) {
+      $vhdType = 'Dynamically Expanding'
+    } else {
+      $vhdType = 'Fixed'
+    }
     $results += New-Object PSObject -Property @{
       "Virtual Machine" = $vm.Name
       "VHD Name" = $vhd.Path
       "VHD Size (GB)" = [Math]::Round($vhd.FileSize/1GB, 2)
-      "VHD Size Used (GB)" = [Math]::Round($vhd.Size/1GB, 2)
-      "VHD Type" = if ($vhd.Dynamic) { "Dynamically Expanding" } else { "Fixed" }
+      "Max VHD Size (GB)" = [Math]::Round($vhd.Size/1GB, 2)
+      "VHD Type" = $vhdType
     }
   }
 }
